44e4ccf12fbede01b0c6b50656ea2c464ba53492
Dawar Rashid <me@dawar.ca>
2023-09-24 17:23:09 -0700

    Update entrypoint.sh to support custom path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# Get NGINX_RELOADED_PATH from ENV or set to default
+CONFIG_PATH=${NGINX_RELOADED_PATH:-'/etc/nginx/nginx-reloaded/nginx.conf'}
+
 # Start the inotifywait loop in the background
-while inotifywait -e modify,delete_self,move_self /etc/nginx/nginx-reloaded/nginx.conf; do
+while inotifywait -e modify,delete_self,move_self $CONFIG_PATH; do
     echo "Detected changes in nginx.conf. Reloading nginx..."
     nginx -s reload
 done &
 
 # Start Nginx in the foreground with the custom config
-exec nginx -g "daemon off;" -c /etc/nginx/nginx-reloaded/nginx.conf
+exec nginx -g "daemon off;" -c $CONFIG_PATH
